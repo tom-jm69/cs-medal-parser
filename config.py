533d490c77934ca948f067ae2e2eb07345c0b4a1
tom-jm03 <Lilleboysweetie@gmail.com>
2025-01-01 22:00:51 +0100

    [BUGFIX] fix badge coins are missing

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 COLLECTIBLES_URL = "https://bymykel.github.io/CSGO-API/api/en/collectibles.json"
 OUTPUT_FOLDER = "data/medals/"
 DUMP_FOLDER = "data/responses/"
-COLLECTIBLE_TYPES = ["pick", "coin", "medal", "pin", "trophy"]
+COLLECTIBLE_TYPES = ["pick", "coin", "medal", "pin", "trophy", "badge"]
